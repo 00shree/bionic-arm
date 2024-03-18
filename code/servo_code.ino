10a8067de7cadf0a80afc7a2eb346cd4e44c6aeb
shree <163702108+90shree@users.noreply.github.com>
2024-03-18 14:28:01 -0400

    Update servo_code.ino

--- code/servo_code.ino
+++ code/servo_code.ino
@@ -3,13 +3,13 @@
 Servo myServo;  // create servo object to control a servo
 
 void setup() {
-  myServo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myServo.attach(9);  // attaches the servo on pin 9 to the servo object, this is where the first index finger servo will be connected
 }
 
 void loop() {
   // Sweep from 0 to 180 degrees
   for (int angle = 0; angle <= 180; angle++) {
-    myServo.write(angle);  // set the servo position
+    myServo.write(angle);  // reset servo position
     delay(10);  // waits for the servo to reach the position
   }
 
